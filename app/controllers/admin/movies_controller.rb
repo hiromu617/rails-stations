40cd4d541a0649dfa6dba70ab91acc75652a5d50
hiro <hiromhiro617@gmail.com>
2021-08-25 11:47:51 +0900

    03

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -5,4 +5,25 @@ class Admin::MoviesController < ApplicationController
 
   def show
   end
+
+  def new
+    @movie= Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to admin_movies_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+  end
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :year, presence: true
+  validates :image_url, presence: true
 end

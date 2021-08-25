ccd555362f44f7d918e2d88cc77e8a292cc67971
hiro <hiromhiro617@gmail.com>
2021-08-25 13:06:10 +0900

    fix

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,43 +1,43 @@
-class Admin::MoviesController < ApplicationController
-  def index
-    @movies = Movie.all
-  end
+# class Admin::MoviesController < ApplicationController
+#   def index
+#     @movies = Movie.all
+#   end
 
-  def show
-  end
+#   def show
+#   end
 
-  def new
-    @movie= Movie.new
-  end
+#   def new
+#     @movie= Movie.new
+#   end
 
-  def create
-    @movie = Movie.new(movie_params)
+#   def create
+#     @movie = Movie.new(movie_params)
 
-    if @movie.save
-      redirect_to admin_movies_path
-    else
-      render 'new'
-    end
-  end
+#     if @movie.save
+#       redirect_to admin_movies_path
+#     else
+#       render 'new'
+#     end
+#   end
 
-  def edit
-    @movie = Movie.find(params[:id])
-  end
+#   def edit
+#     @movie = Movie.find(params[:id])
+#   end
 
-  def update
-    @movie = Movie.find(params[:id])
+#   def update
+#     @movie = Movie.find(params[:id])
 
-    if @movie.update(movie_params)
-      redirect_to admin_movies_path
-    else
-      render 'edit'
-    end
-  end
+#     if @movie.update(movie_params)
+#       redirect_to admin_movies_path
+#     else
+#       render 'edit'
+#     end
+#   end
 
-  private
+#   private
 
-  def movie_params
-    params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
-  end
+#   def movie_params
+#     params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+#   end
 
-end
+# end

0642a02be46491e7f59a04a9a2bbaed86d5cf3dc
hiro <hiromhiro617@gmail.com>
2021-08-25 11:51:12 +0900

    fix

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,5 +7,22 @@ class MoviesController < ApplicationController
   end
 
   def new
+    @movie= Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to admin_movies_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
   end
 end

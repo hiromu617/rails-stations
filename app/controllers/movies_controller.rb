2c58aea3970b1d4af08f8ebba00f9a270aa69838
hiro <hiromhiro617@gmail.com>
2021-08-25 12:40:48 +0900

    fix

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,6 +20,20 @@ class MoviesController < ApplicationController
     end
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    if @movie.update(movie_params)
+      redirect_to admin_movies_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def movie_params
